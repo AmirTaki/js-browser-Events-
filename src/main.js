e5a3414ac8f5f9939e6e50853b09f0bc69201971
amir <amirtaki623@gmail.com>
2025-05-11 17:01:41 +0330

    Event hierarchy : event

--- src/main.js
+++ src/main.js
@@ -1,52 +1,18 @@
-// document.getElementById("text").innerHTML = " event Bubbling : EVENT?"
+// document.getElementById("text").innerHTML = " Event hierarchy: EVENT?"
 
-document.querySelector("ul").addEventListener("click", ()=>{
-    console.log("ul")  // child
+document.querySelector("ul").addEventListener("click", (e)=>{
+    console.log(e)  // => return objectEvent : mouseEvent  -> (inherit)MouseEvent -> (inherit)UIEvent -> (inherit) Event
 })
 
-document.querySelector("ul li").addEventListener("click", ()=>{
-    console.log("ul li")  // child -> parent
+/// property type for object Event  because (inherit) pass object.Event property type 
+
+document.querySelector("#main-title").addEventListener("click", (e)=>{
+    console.log(e.type)  
 })
 
-document.querySelector("ul li span").addEventListener("click", ()=>{
-    console.log("ul li span") // child > parent(child)-> parent 
+
+// load event
+window.addEventListener('load', (event)=>{
+    console.log(event)  // => return type event
+    
 })
-
-/* -------------------------------------------------------------------- */
-
-//1- event capturing (parent -> child) : {capture : true}
-//2- event Bubbling  (child -> parent) : default : {capture : false}
-
-/*
-capturing : ture , bubling : false
- capturing  v|  1   ul    6 |^ bubbling
-            v|              |^    
-            v|  2   li    5 |^
-            v|              |^
-            v|  3   span  4 |^
-
-*/
-
-document.querySelector("ul").addEventListener("click", ()=>{
-    console.log("ul")  // child
-},{capture:false})
-
-document.querySelector("ul li").addEventListener("click", (event)=>{
-    event.stopPropagation() // pause : capturing
-    console.log("li")  // child -> parent
-}, {capture:false})
-
-document.querySelector("ul li span").addEventListener("click", ()=>{
-    console.log("span") // child > parent(child)-> parent 
-}, {capture:false})
-
-// method :stopPropagation() => stop : bubbling OR capturing
-
-/* -------------------------------------------------------------------- */
-
-// stop click 
-document.querySelector("#main-title").addEventListener("click", ()=>{
-    console.log('mian - title')
-},{once:true})  // => stop eventListener -> click 
-
-/* -------------------------------------------------------------------- */
