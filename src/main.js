c577d717c4273cefcf07a183333051aa22c0e77f
amir <amirtaki623@gmail.com>
2025-05-11 16:04:18 +0330

    What is an Event ? & attribute onClick & event click

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
-// document.getElementById("text").innerHTML = "animation : EVENT "
+// document.getElementById("text").innerHTML = "What is an EVENT?"
 
-// animation start
-document.querySelector("#main-title").addEventListener("animationstart",(e)=>{
-    console.log("animationStart:", e)
-})
+//  Event Click  => attribute onClick
 
-// animation end
-document.querySelector("#main-title").addEventListener("animationend",(e)=>{
-    console.log("animationEnd:", e)
+let clickEvent = () => {
+    console.log("clicEvent")
+    document.querySelector("footer").remove()
+}
+
+
+// event : 1- Occurs by the user  =>  Event click
+// event : 2- Depending on what happens in the browser  => Event load
+
+// addEventListener  => event : click
+
+// document.querySelector('li').addEventListener("click", clickEvent)
+
+document.querySelectorAll('li')[1].addEventListener("click", (e)=>{
+        document.querySelector("footer").remove()  
 })
