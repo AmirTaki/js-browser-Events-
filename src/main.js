cf9112c3535ca3b7ab1cb310e2bc219db1c6e950
amir <amirtaki623@gmail.com>
2025-05-11 23:21:26 +0330

    transition event

--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
-// document.getElementById("text").innerHTML = "clipboard event"
+// document.getElementById("text").innerHTML = "transition event "
 // 
 
-document.querySelector("#myform input[type=text]").addEventListener("copy", (e)=>{
-    console.log('copy', e)
-})
 
-document.querySelector("#myform input[type=text]").addEventListener("cut", (e)=>{
-    console.log('cut', e)
-})
-
-document.querySelector("#myform input[type=text]").addEventListener("paste", (e)=>{
-    console.log('paste', e)
+document.querySelector('#main-title').addEventListener("transitionend", (e)=>{
+    // console.log(e)
+    console.log(e.propertyName)  // property 
 })
