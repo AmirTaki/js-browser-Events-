e056263f7d5d929a02b407b35bc0fdf40e313d08
amir <amirtaki623@gmail.com>
2025-05-11 17:38:44 +0330

    scroll event

--- src/main.js
+++ src/main.js
@@ -1,16 +1,8 @@
-// document.getElementById("text").innerHTML = " orientation Change : EVENT?"
+// document.getElementById("text").innerHTML = " scroll : EVENT?"
 
 
-// screen -> orientation.type
+// 
 
-window.screen.orientation.type  // => return : landscape - primary
-
-// event : orientation change
-
-window.addEventListener("orientationchange" ,(e)=>{
-    console.log(window.screen.orientation)
-    console.log(window.screen.orientation.type)
-
-    document.body.style.backgroundColor = window.screen.orientation.type == "landscape-primary" ? "red" : 'blue'
+window.addEventListener('scroll', function(e){
+    console.log(this.document.documentElement.scrollTop)  // => return  distance scroll 
 })
-   
