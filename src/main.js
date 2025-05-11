49953834b908cb7e8481f8c7129a7ee0448ce5ea
amir <amirtaki623@gmail.com>
2025-05-11 23:32:36 +0330

    toturial event in browsers js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,1 @@
-// document.getElementById("text").innerHTML = "transition event "
-// 
-
-
-document.querySelector('#main-title').addEventListener("transitionend", (e)=>{
-    // console.log(e)
-    console.log(e.propertyName)  // property 
-})
+// document.getElementById("text").innerHTML = "event  addeventListener"
