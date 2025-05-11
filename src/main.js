8cd5733194d167ff9c3266641bd2a258a80d41de
amir <amirtaki623@gmail.com>
2025-05-11 22:38:05 +0330

    focus event

--- src/main.js
+++ src/main.js
@@ -1,30 +1,12 @@
-// document.getElementById("text").innerHTML = "input event
+// document.getElementById("text").innerHTML = "focus event
 // 
 
-document.querySelector("#myform input[type=text]").addEventListener("input", (e)=>{
-    // console.log(e)
-    // console.log('input')
+document.querySelector("#myform input[type=text]").addEventListener("focus", (e)=>{
+    console.log('focus')
+    console.log(e.relatedTarget)
 })
 
-document.querySelector("#myform input[type=text]").addEventListener("beforeinput", (e)=>{
-    // console.log(e)
-    // console.log("beforeinput")
-})
-
-// property
-
-document.querySelector("#myform input[type=text]").addEventListener("input", (e)=>{
-    
-    console.log(e.data)
-
-    console.log(e.inputType)
-
-    console.log(e.target)
-
-    console.log(e.target.value)
-})
-
-
-document.querySelector("#myform input[type=text]").addEventListener("beforeinput", (e)=>{
-   e.data == 0  ? e.preventDefault() : ""
+document.querySelector("#myform input[type=text]").addEventListener("blur", (e)=>{
+    console.log('blur')
+    console.log(e.relatedTarget)
 })
