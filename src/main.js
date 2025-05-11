5acbfc2ccd2cbbf54e9616e1876826ae2d0b4559
amir <amirtaki623@gmail.com>
2025-05-11 23:02:10 +0330

    drag event

--- src/main.js
+++ src/main.js
@@ -1,12 +1,46 @@
-// document.getElementById("text").innerHTML = "focus event
+// document.getElementById("text").innerHTML = "drag event
 // 
 
-document.querySelector("#myform input[type=text]").addEventListener("focus", (e)=>{
-    console.log('focus')
-    console.log(e.relatedTarget)
+
+document.querySelector("#main-title").addEventListener("dragstart", (e)=>{
+    console.log("dragStart")  // return => drag event
 })
 
-document.querySelector("#myform input[type=text]").addEventListener("blur", (e)=>{
-    console.log('blur')
-    console.log(e.relatedTarget)
+document.querySelector("#main-title").addEventListener("dragend", (e)=>{
+    console.log("dragEnd")  // return => drag event
+})
+
+document.querySelector("#main-title").addEventListener("drag", (e)=>{
+    console.log("drag")  // return => drag event
+})
+
+
+
+// property  
+/*
+   event.dataTransfer.setDaa("key","value") => drag start
+ 
+ event.preventDefault() => drag over
+
+ event.dataTransfer.getData('key')  => drop
+
+*/
+
+
+document.querySelector("#main-title").addEventListener("dragstart", (e)=>{
+    console.log("dragStart")
+   e.dataTransfer.setData("myDrag", e.target.id)
+})
+
+document.querySelector("header").addEventListener("dragover", (e)=>{
+    console.log("dragover")
+    e.preventDefault()
+    
+})
+document.querySelector("header").addEventListener("drop", (e)=>{
+    console.log('drop')
+    let dataid = e.dataTransfer.getData('myDrag')
+    console.log(document.getElementById(dataid))
+    e.target.appendChild(document.getElementById(dataid))
+
 })
