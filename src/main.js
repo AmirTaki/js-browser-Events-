d62c60ed892cac81a8925ac0f4e7c8a69bbf8403
amir <amirtaki623@gmail.com>
2025-05-11 17:49:52 +0330

    Personal Event : event

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-// document.getElementById("text").innerHTML = " scroll : EVENT?"
+// document.getElementById("text").innerHTML = " Personal event : EVENT?"
 
 
-// 
+// Custom Event (name, {object}  )
 
-window.addEventListener('scroll', function(e){
-    console.log(this.document.documentElement.scrollTop)  // => return  distance scroll 
+
+let myEvent = new CustomEvent("personalEvent", {detail: Date.now()})
+
+// handel event 
+
+document.querySelector("main").addEventListener("personalEvent", (e)=>{
+    e.target.style.backgroundColor = "red"
+})
+
+window.addEventListener("scroll", (e)=>{
+    if(this.document.documentElement.scrollTop >= 214){
+        document.querySelector("main").dispatchEvent(myEvent)
+    } 
 })
